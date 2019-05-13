0522ce3e490082e63ee77b344b9de283aa9f60f3
network2501 <pete@network2501.com>
2019-05-13 18:36:19 +0800

    bot.py emoji functions

--- bot.py
+++ bot.py
@@ -3,13 +3,17 @@
 
 import settings
 import discord
+import functools
+import inspect
+import typing
+
 from discord.ext import commands
 import json
+from discord.utils import get
 from functools import partial
 
-
 # Create variable json_data assign the opening of a file to it
-ships_json=open('/Users/peter.carstairs/scripts/apex-bot/res/ships.json')
+ships_json = open('/Users/peter.carstairs/scripts/apex-bot/res/ships.json')
 print(ships_json)
 ships_data = json.load(ships_json)
 
@@ -21,46 +25,13 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
-#@bot.command(pass_context=True)
-#async def test(ctx, arg1):
-#    await ctx.send('here {}'.ships)
-
-#def ship_title(arcg1):
-#   for element in ships_data[arg1]:
-#       ship_name = element['ship_name']
-#       rarity = element['r']
-#       ship_embed_title = ship_name + " " + rarity
-#       return ship_embed_title
-#
-#def ship_description(arg1):
-#   for element in ships_data[arg1]:
-#       ship_name = element['ship_name']
-#       damage_output = str(element['damage output'])
-#       damage_type = element['damage type']
-#       weapon_name = element['weapon_name']
-#       rarity = element['r']
-#       ship_stats_embed_description = damage_output + damage_type + weapon_name
-#       return ship_stats_embed_description
-#
-#def ship_aura(arg1):
-#   for element in ships_data[arg1]:
-#       aura = element['aura']
-#       return aura
-#
-#def ship_zen(arg1):
-#   for element in ships_data[arg1]:
-#       zen = element['zen']
-#       return zen
-
-
-
-def ship_stat(ship_name, stat_name):    # <-- note, you don't have to call it arg1 in the other functions, they can
-                                        # make up their own name for it that will only be used within that function,
-                                        # so the code is more descriptive returns the first seen stat for the given
-                                        # ship name and stat name
-   for element in ships_data[ship_name]:
-       stat_value = element[stat_name]
-       return stat_value
+def ship_stat(ship_name, stat_name):  # <-- note, you don't have to call it arg1 in the other functions, they can
+    # make up their own name for it that will only be used within that function,
+    # so the code is more descriptive returns the first seen stat for the given
+    # ship name and stat name
+    for element in ships_data[ship_name]:
+        stat_value = element[stat_name]
+        return stat_value
 
 
 def em_colour(type):
@@ -72,17 +43,44 @@ def em_colour(type):
         return 0xffb820
 
 
+def em_emojidmg(type):
+    emojisb = "<:sb:572354637336936448>"
+    emojihi = "<:hi:572354637043335169>"
+    emojiap = "<:ap:572354637320159252>"
+    if type == "sb":
+        return emojisb
+    elif type == "hi":
+        return emojihi
+    elif type == "ap":
+        return emojiap
+
+
+def em_emojirarity(type):
+    emojicommon = "<:common:576427314368217128>"
+    emojirare = "<:rare:576427437831880714>"
+    emojisuperrare = "<:superrare:576427437844332544>"
+    if type == "common":
+        return emojicommon
+    elif type == "rare":
+        return emojirare
+    elif type == "superrare":
+        return emojisuperrare
+
 client = MyClient()
 bot = commands.Bot(command_prefix='!')
 
 
+def customemoji(search):
+    iconemoji = discord.utils.get(emojis(), name=search)
+    return iconemoji
+
 @bot.command()
-async def ships(ctx, ship_name):
-    ship_embed_title = ship_stat(ship_name, "rarity") + " " + ship_stat(ship_name, "ship_name")
-    ship_embed_description = ship_stat(ship_name, "aura")
-    ship_embed_zen = ship_stat(ship_name, "zen")
-    ship_embed_aura = ship_stat(ship_name, "aura")
-    embed_colour = em_colour(ship_stat(ship_name, "damage_type"))
+async def ships(ctx, arg1):
+    ship_embed_title = em_emojirarity(ship_stat(arg1, "rarity")) + " " + ship_stat(arg1, "ship_name")
+    ship_embed_description = em_emojidmg(ship_stat(arg1, "damage_type")) + " " + str(ship_stat(arg1, "damage_output"))
+    ship_embed_zen = ship_stat(arg1, "zen")
+    ship_embed_aura = ship_stat(arg1, "aura")
+    embed_colour = em_colour(ship_stat(arg1, "damage_type"))
     embed = discord.Embed(title=ship_embed_title, description=ship_embed_description, colour=embed_colour)
     embed.add_field(name="Aura", value=ship_embed_aura, inline=False)
     embed.add_field(name="Zen", value=ship_embed_zen, inline=False)
@@ -90,16 +88,17 @@ async def ships(ctx, ship_name):
     return
 
 
-@bot.command()
-async def invaders(ctx, arg1):
-    for element in jdata[arg1]:
-        await ctx.send(element['ship_name'] + "\n" + \
-                        element['damage type'] + "\n" + \
-                        str(element['damage output']) + "\n" + \
-                        element['weapon_name'] + "\n" + \
-                        element['aura'] + "\n" + \
-                        element['zen'] + "\n" + \
-                        ":" + element['r'] + ":")
-        return
+#@bot.command()
+#async def invaders(ctx, arg1):
+#    for element in jdata[arg1]:
+#        await ctx.send(element['ship_name'] + "\n" + \
+#                       element['damage type'] + "\n" + \
+#                       str(element['damage output']) + "\n" + \
+#                       element['weapon_name'] + "\n" + \
+#                       element['aura'] + "\n" + \
+#                       element['zen'] + "\n" + \
+#                       ":" + element['r'] + ":")
+#        return
+
 
 bot.run(settings.discordkey)
