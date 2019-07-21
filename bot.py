b5a4925758fbe3712385832035e050c2659cdb01
network2501 <pete@network2501.com>
2019-07-22 00:55:22 +1000

    removed duplicated uses of ship_search()

--- bot.py
+++ bot.py
@@ -55,37 +55,11 @@ def ship_stat(find_ship, stat_name):
 # Discord embed have a line of the left hand side. This line can be coloured
 # This function uses one of the 3 damage_type choices to select a colour from
 # the embed_colours variable (red/yellow/blue)
-def get_em_colour(arg1):
-    ship_dict = ships_data[ship_search(arg1)]
+def get_em_colour(ship_name):
+    ship_dict = ships_data[ship_name]
     dmg_type = ship_dict["damage_type"]
     em_colour = embed_colours[dmg_type]
     return em_colour
-
-# This is the small embed description output used by the
-#  "!ship info <ship name>" command. 
-def get_ship_description_small(arg1):
-    ship_dict = ships_data[ship_search(arg1)]
-    ship_description_small = ("{emojidps} DPS {dpsvalue}\n"
-    "{emojidmgtype} Weapon {weaponname}\n"
-    "{emojiaura} Aura {auraname}\n"
-    "{emojizen} Zen {zenname}\n").format(emojidps=emoji("dps"),
-            dpsvalue=str(ship_dict["damage_output"]),
-            emojidmgtype=emoji(ship_dict["damage_type"]),
-            weaponname=(ship_dict["weapon_name"]),
-            emojiaura=emoji(ship_dict["aura"]),
-            auraname=ship_dict["aura"],
-            emojizen=emoji(ship_dict["zen"]),
-            zenname=ship_dict["zen"])
-    return ship_description_small
-
-# Creates the title of the discord emebed consisting of the rarity emoji 
-# the ship name.
-def get_ship_title(arg1):
-    ship_dict = ships_data[ship_search(arg1)]
-    ship_title = ("{rarityemoji} {nameofship}").format(\
-        rarityemoji=emoji(ship_dict["rarity"]), 
-        nameofship=ship_dict["ship_name"])
-    return ship_title
 
 # Receives the element from ships.json maybe another file in 
 # future and uses it as a key in emoji.json.
@@ -93,9 +67,32 @@ def emoji(key):
     emoji = emoji_data[key]
     return emoji
 
-def get_ship_image(arg1):
+# This is the small embed description output used by the
+#  "!ship info <ship name>" command. 
+def get_ship_description_small(ship_name):
+    ship_dict = ships_data[ship_name]
+    ship_description_small = ("{emojidps} {ship[damage_output]}\n"
+    "{emojidmgtype} {ship[weapon_name]}\n"
+    "{emojiaura} {ship[aura]}\n"
+    "{emojizen} {ship[zen]}\n").format(emojidps=emoji("dps"),
+            emojidmgtype=emoji(ship_dict["damage_type"]),
+            emojiaura=emoji(ship_dict["aura"]),
+            emojizen=emoji(ship_dict["zen"]),
+            ship=ship_dict)
+    return ship_description_small
+
+# Creates the title of the discord emebed consisting of the rarity emoji 
+# the ship name.
+def get_ship_title(ship_name):
+    ship_dict = ships_data[ship_name]
+    ship_title = ("{rarityemoji} {nameofship}").format(\
+        rarityemoji=emoji(ship_dict["rarity"]), 
+        nameofship=ship_dict["ship_name"])
+    return ship_title
+
+def get_ship_image(ship_name):
     urlgit = "https://github.com/Phoenix-II-Community/apex-bot/raw/master/img/"
-    url = ("{giturl}{shipname}.png").format(giturl=urlgit, shipname=ship_search(arg1))
+    url = ("{giturl}{shipname}.png").format(giturl=urlgit, shipname=ship_name)
     return url
 
 ################
@@ -115,7 +112,6 @@ async def bday(ctx):
         difference = future - present
         await ctx.send(difference)
 
-
 @bot.group()
 async def ship(ctx):
     if ctx.invoked_subcommand is None:
@@ -126,11 +122,12 @@ async def ship(ctx):
 # A 5 line embed with basic info: name, weapon, dps, aura and zen.
 @ship.command()
 async def info(ctx, *, arg1):
-    ship_embed_title = get_ship_title(arg1)
-    ship_embed_description = get_ship_description_small(arg1)
-    embed_colour = get_em_colour(arg1)
+    ship_name = ship_search(arg1)
+    ship_embed_title = get_ship_title(ship_name)
+    ship_embed_description = get_ship_description_small(ship_name)
+    embed_colour = get_em_colour(ship_name)
     embed = discord.Embed(title=ship_embed_title, description=ship_embed_description, colour=embed_colour)
-    embed.set_thumbnail(url=get_ship_image(arg1))
+    embed.set_thumbnail(url=get_ship_image(ship_name))
     await ctx.send(embed=embed)
 
 #@info.error
