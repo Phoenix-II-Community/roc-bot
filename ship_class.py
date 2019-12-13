1444ee47a7ac006af76a8f349fbafd9ddddf4c61
network2501 <pete@network2501.com>
2019-12-13 22:29:13 +0800

    Ship class is working decently now

--- ship_class.py
+++ ship_class.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from common import customemoji, ship_search
+from common import customemoji, ship_search, sanitise_input
 import sqlite3 
 import re
 import discord.ext.commands
 from discord.ext.commands import Bot
+import urllib.parse
 
 # This replaces the old ship.py 
-class ShipInfo(Bot):
+class Ship(Bot):
     def __init__(self, bot_self, find_this):
         self.bot_self = bot_self
         self.ship_name = ship_search(find_this)
         self.s_obj = self.sql_ship_obj()
-        self.embed = self.create_emebed()
-        self.embed_title = self.get_ship_title()
-        self.embed_description = self.get_ship_description_info()
-        self.ship_image = self.get_ship_image()
+        self.img_url = self.get_ship_image()
 
     def sql_ship_obj(self):
         # connect to the sqlite database
@@ -26,35 +24,40 @@ class ShipInfo(Bot):
         # make an sqlite connection object
         c = conn.cursor()
         # using a defined view s_info find the ship 
-        c.execute('select * from s_info where name = ?', self.ship_name)
+        c.execute('select * from s_info where name = ?', [self.ship_name])
         # return the ship object including the required elemnts
-        ship_obj = c.fetchone()
+        s_obj = c.fetchone()
         # close the databse connection
         conn.close()
         # return the sqlite3.cursor object
-        return ship_obj
+        return s_obj
 
     # Creates the title of the discord emebed consisting of the rarity emoji 
     # the ship name.
     def get_ship_title(self):
-        ship_title = (f"{customemoji(self.bot_self, self.s_obj['rarity'])} {self.s_obj['name']}")
-        return ship_title
+        embed_title = (
+            f"{customemoji(self.bot_self, self.s_obj['rarity'])} {self.s_obj['name']}")
+        return embed_title
     
     def get_ship_description_info(self):
-        ship_description_info = (
+        embed_description = (
             f"{customemoji(self.bot_self, 'dps')} {self.s_obj['dmg']}\n"
             f"{customemoji(self.bot_self, self.s_obj['affinity'])} {self.s_obj['weapon_name']}\n" 
             f"{customemoji(self.bot_self, self.s_obj['aura'])} {self.s_obj['aura']}\n"
-            f"{customemoji(self.bot_self, self.s_obj['zen'])} {self.s_obj['zen']}\n")
-        return ship_description_info
+            f"{customemoji(self.bot_self, self.s_obj['zen'])} {self.s_obj['zen']}")
+        return embed_description
     
     def get_ship_image(self):
         urlgit = "https://github.com/Phoenix-II-Community/apex-bot/raw/master/img/"
-        url = ("{giturl}{shipname}.png").format(giturl=urlgit, shipname=self.ship_name)
-        return url
+        img_url = (f"{urlgit}{sanitise_input(self.ship_name.lower())}.png")
+        return img_url
 
-    def create_emebed(self):
-        embed = discord.Embed(title=self.embed_title, 
-        description=self.embed_description, colour=self.s_obj['colour'])
-        embed.set_thumbnail(url=self.ship_image)
-        return embed
+# create a discod embed object. Using the Ship class to collection the required
+def info_embed(bot_self, find_this):
+    thing = Ship(bot_self, find_this)
+    title = thing.get_ship_title()
+    desc = thing.get_ship_description_info()
+    col = int(thing.s_obj['colour'], 16)
+    return discord.Embed(title=title, 
+    description=desc, colour=col).set_thumbnail(url=thing.img_url)
+
